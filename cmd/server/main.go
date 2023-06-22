f6827d56ce40c8178df8dd31fcf9fa3b07b1637b
Andre Machado <andre.machado@crowdbotics.com>
2023-06-21 21:21:30 -0400

    Add initial entrypoint for the app

---
+++ cmd/server/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// Run is going to be responsible for the instantiation and start of our
+// go application
+func Run() error {
+    fmt.Println("Starting up our application")
+	return nil
+}
+
+func main() {
+	fmt.Println("Go REST API Course")
+    if err := Run(); err != nil {
+        fmt.Println(err)
+    }
+}
